8cfbd1a25097dcd5a967abac5b569bb5384a0a53
Hasubaru <sewasi12345@gmail.com>
2025-11-07 20:40:06 +0700

    time

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,21 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.ShaderGraph;
 using UnityEngine;
 
 public class PlayerController : Singleton<PlayerController>
 {
-   /* public void EnableControls()
+
+    public void EnableControls()
     {
-        controls.Enable();
+        playerControls.Enable();
     }
     public void DisableControls()
     {
-        controls.Disable();
+        playerControls.Disable();
         valueX = 0;
-;    }*/
+    }
     public bool FacingLeft { get { return facingLeft; } }
 
-    public float controls;
+    
+    private float valueX;
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float dashSpeed = 4f;
     [SerializeField] private TrailRenderer myTrailRenderer;
