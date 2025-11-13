f2e76df776df84a2e072c4228000175e4f79f7f0
Hasubaru <sewasi12345@gmail.com>
2025-11-13 17:18:28 +0700

    fixcutscene + skip

---
+++ Assets/Scripts/AMinh/CutsceneSkip.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneSkip : MonoBehaviour
+{
+    public PlayableDirector director;      // Timeline của cutscene
+    public GameObject cutsceneUI;          // UI đang hiện khi cutscene
+    public PlayerController player;        // player để bật điều khiển lại
+
+    public void Skip()
+    {
+        // tua tới cuối timeline
+        director.time = director.duration;
+        director.Evaluate();
+        director.Stop();
+
+        // tắt UI cutscene
+        if (cutsceneUI != null) cutsceneUI.SetActive(false);
+
+        // bật điều khiển lại
+        if (player != null) player.EnableControls();
+    }
+}
