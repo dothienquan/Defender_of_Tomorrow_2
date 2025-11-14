884d98e110590c51e070c64339d7c6d02b9d8dd5
HungLam202 <kalinatribaricanxi@gmail.com>
2025-11-15 05:15:49 +0700

    hint minigame

---
+++ Assets/Scripts/Player/ImageInteractable.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageInteractable : Interactable
+{
+    [Header("UI Image to Toggle")]
+    [SerializeField] private GameObject imagePanel;
+
+    private bool isShowing = false;
+
+    private void Awake()
+    {
+        if (imagePanel != null)
+        {
+            imagePanel.SetActive(false); // Ẩn ban đầu
+        }
+    }
+
+    public override void Interact(GameObject interactor)
+    {
+        if (imagePanel == null) return;
+
+        isShowing = !isShowing;
+        imagePanel.SetActive(isShowing);
+    }
+}
