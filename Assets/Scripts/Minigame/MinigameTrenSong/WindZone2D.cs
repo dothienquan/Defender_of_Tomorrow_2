f678241d1fbf5306457eeb2f5c97a3361b55d903
HungLam202 <kalinatribaricanxi@gmail.com>
2025-11-25 22:01:47 +0700

    mini game map nui

---
+++ Assets/Scripts/Minigame/MinigameTrenSong/WindZone2D.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class WindZone2D : MonoBehaviour
+{
+    [Header("Wind Settings")]
+    public Vector2 windDirection = new Vector2(1f, 0f); // hướng gió (mặc định sang phải)
+    public float windStrength = 3f;                      // độ mạnh (units/second)
+
+    private void Reset()
+    {
+        // đảm bảo collider là trigger
+        var col = GetComponent<Collider2D>();
+        col.isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        var player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            Vector2 wind = windDirection.normalized * windStrength;
+            player.SetWind(wind);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        var player = other.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.SetWind(Vector2.zero);
+        }
+    }
+}
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,16 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : Singleton<PlayerController>
 {
     public bool FacingLeft { get { return facingLeft; } }
-    
+
 
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float dashSpeed = 4f;
     [SerializeField] private TrailRenderer myTrailRenderer;
     [SerializeField] private Transform weaponCollider;
+
+    // WIND: vận tốc gió hiện tại tác dụng lên player (world space, units/second)
+    private Vector2 currentWind = Vector2.zero;
 
     private PlayerControls playerControls;
     private Vector2 movement;
@@ -23,7 +26,8 @@ public class PlayerController : Singleton<PlayerController>
     private bool facingLeft = false;
     private bool isDashing = false;
 
-    protected override void Awake() {
+    protected override void Awake()
+    {
         base.Awake();
 
         playerControls = new PlayerControls();
@@ -33,7 +37,8 @@ public class PlayerController : Singleton<PlayerController>
         knockback = GetComponent<Knockback>();
     }
 
-    private void Start() {
+    private void Start()
+    {
         playerControls.Combat.Dash.performed += _ => Dash();
 
         startingMoveSpeed = moveSpeed;
@@ -41,55 +46,74 @@ public class PlayerController : Singleton<PlayerController>
         ActiveInventory.Instance.EquipStartingWeapon();
     }
 
-    private void OnEnable() {
+    private void OnEnable()
+    {
         playerControls.Enable();
     }
 
-    private void OnDisable() {
+    private void OnDisable()
+    {
         playerControls.Disable();
     }
 
-    private void Update() {
+    private void Update()
+    {
         PlayerInput();
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
         AdjustPlayerFacingDirection();
         Move();
     }
 
-    public Transform GetWeaponCollider() {
+    public Transform GetWeaponCollider()
+    {
         return weaponCollider;
     }
 
-    private void PlayerInput() {
+    private void PlayerInput()
+    {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
 
         myAnimator.SetFloat("moveX", movement.x);
         myAnimator.SetFloat("moveY", movement.y);
     }
 
-    private void Move() {
+    private void Move()
+    {
         if (knockback.GettingKnockedBack || PlayerHealth.Instance.isDead) { return; }
 
-        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+        // vận tốc do input
+        Vector2 inputVelocity = movement * moveSpeed;
+
+        // tổng: input + gió
+        Vector2 finalVelocity = inputVelocity + currentWind;
+
+        rb.MovePosition(rb.position + finalVelocity * Time.fixedDeltaTime);
     }
 
-    private void AdjustPlayerFacingDirection() {
+    private void AdjustPlayerFacingDirection()
+    {
         Vector3 mousePos = Input.mousePosition;
         Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
 
-        if (mousePos.x < playerScreenPoint.x) {
+        if (mousePos.x < playerScreenPoint.x)
+        {
             mySpriteRender.flipX = true;
             facingLeft = true;
-        } else {
+        }
+        else
+        {
             mySpriteRender.flipX = false;
             facingLeft = false;
         }
     }
 
-    private void Dash() {
-        if (!isDashing && Stamina.Instance.CurrentStamina > 0) {
+    private void Dash()
+    {
+        if (!isDashing && Stamina.Instance.CurrentStamina > 0)
+        {
             Stamina.Instance.UseStamina();
             isDashing = true;
             moveSpeed *= dashSpeed;
@@ -98,7 +122,8 @@ public class PlayerController : Singleton<PlayerController>
         }
     }
 
-    private IEnumerator EndDashRoutine() {
+    private IEnumerator EndDashRoutine()
+    {
         float dashTime = .2f;
         float dashCD = .25f;
         yield return new WaitForSeconds(dashTime);
@@ -106,5 +131,11 @@ public class PlayerController : Singleton<PlayerController>
         myTrailRenderer.emitting = false;
         yield return new WaitForSeconds(dashCD);
         isDashing = false;
+    }
+
+    // WIND: hàm cho vùng gió gọi vào
+    public void SetWind(Vector2 wind)
+    {
+        currentWind = wind;
     }
 }
