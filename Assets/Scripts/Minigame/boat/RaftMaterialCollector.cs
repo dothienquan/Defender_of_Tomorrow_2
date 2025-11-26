42ce29d9d10ebf021a26f28240b7877104399018
HungLam202 <kalinatribaricanxi@gmail.com>
2025-11-26 22:00:24 +0700

    mini game craft boat

---
+++ Assets/Scripts/Minigame/boat/RaftMaterialCollector.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class RaftMaterialCollector : MonoBehaviour
+{
+    [Header("Material Settings")]
+    public int requiredWood = 10;     // số gỗ cần để được chơi mini game
+    public int currentWood = 0;       // đang có bao nhiêu gỗ
+
+    public bool HasEnoughMaterials => currentWood >= requiredWood;
+
+    // Gọi hàm này khi player nhặt được 1 nguyên liệu (vd: gỗ)
+    public void AddMaterial(int amount)
+    {
+        currentWood += amount;
+        Debug.Log($"Wood: {currentWood}/{requiredWood}");
+    }
+}
