6f13c3d609eeac595b1727cfe8876ec961703a2c
Hasubaru <sewasi12345@gmail.com>
2025-12-21 19:15:08 +0700

    Fade

---
+++ Assets/Scripts/AMinh/CreditScroll.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class CreditScroll : MonoBehaviour
+{
+    public TMP_Text creditText;
+
+    public float scrollDuration = 25f;
+    public float extraOffset = 500f;
+
+    [TextArea(10, 50)]
+    [SerializeField]
+    private string creditContent =
+@"DIRECTOR
+Nguyen Van A
+
+PROGRAMMER
+Le Minh B
+
+ART & DESIGN
+Tran Thi C
+
+SPECIAL THANKS
+You ❤️
+";
+
+    Tween scrollTween;
+
+    public void PlayCredit()   // ← Timeline sẽ gọi hàm này
+    {
+        creditText.text = creditContent;
+
+        RectTransform rt = creditText.rectTransform;
+        rt.anchoredPosition = Vector2.zero;
+
+        float distance = creditText.preferredHeight + extraOffset;
+
+        scrollTween = rt
+            .DOAnchorPosY(distance, scrollDuration)
+            .SetEase(Ease.Linear);
+    }
+}
