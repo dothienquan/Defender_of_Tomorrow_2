32314b52cf936e89251fce7be8739a27a06ef760
HungLam202 <kalinatribaricanxi@gmail.com>
2025-12-22 11:12:37 +0700

    update

---
+++ Assets/Scripts/PlaySoundOnDisable.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundOnDisable : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnDisable()
+    {
+        if (audioSource != null && audioSource.clip != null)
+        {
+            // Tạo audio tạm để không bị cắt khi object tắt
+            GameObject temp = new GameObject("Temp_Audio");
+            AudioSource tempSource = temp.AddComponent<AudioSource>();
+            tempSource.clip = audioSource.clip;
+            tempSource.volume = audioSource.volume;
+            tempSource.Play();
+
+            Destroy(temp, audioSource.clip.length);
+        }
+    }
+}
